74cf60378de6d97af754dc77b40a980d066baeb7
Russell Barr <rbarrsquared@gmail.com>
2021-07-27 15:14:33 -0500

    Part 1, q4

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-let favFoods = ['sushi', 'tacos', 'ramen', 'pizza']
+let favFoods = ['sushi', 'tacos', 'ramen', 'pizza'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
-console.log("Favorite foods are: " + favFoods);
+console.log('Favorite foods are: ' + favFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
@@ -49,8 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
-
+favFoods.push('gyros');
+console.log(favFoods);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -59,14 +59,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
-
+let removedFood = favFoods.pop();
+console.log('Removed last food: ' + removedFood);
+console.log('The foods are now: ' + favFoods);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+favFoods.unshift('egg rolls');
+console.log('Added a food to beginning: ${favFoods}');
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -75,3 +78,6 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+let firstFood = favFoods.shift();
+console.log('Food removed: ' + firstFood);
+console.log('Updated food list: ' + favFoods);
